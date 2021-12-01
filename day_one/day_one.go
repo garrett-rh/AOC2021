f48aee9f1dcac459d6f6fc2e99680c5b82bdb4ec
Garrett Harvey <garretth@tuta.io>
2021-12-01 15:58:46 -0500

    Day one completed.

---
+++ day_one/day_one.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func read_file() (file_lines []int64) {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalf("unable to open file: %v", err)
+	}
+
+	scanner := bufio.NewScanner(file) // Takes the pointer created by os.Open
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		int1, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+		file_lines = append(file_lines, int1)
+	}
+	file.Close()
+
+	return
+}
+
+func part1_solution(txtlines []int64) (part_one_count int) {
+	part_one_count = 0
+	for idx, line := range txtlines {
+		if idx == 0 { // Prevents out of range indexing
+			continue
+		} else if line > txtlines[idx-1] {
+			part_one_count += 1
+		}
+	}
+	return
+}
+
+func part2_solution(txtlines []int64) (part_two_count int) {
+	var last_three_line_sum int64
+	var new_three_line_sum int64
+	for idx, line := range txtlines {
+		if idx == 0 { // Prevents out of range indexing
+			continue
+		} else if idx > (len(txtlines) - 3) { // Breaks if there aren't enough measurements left
+			break
+		} else {
+			new_three_line_sum = line + txtlines[idx+1] + txtlines[idx+2]
+			last_three_line_sum = txtlines[idx-1] + line + txtlines[idx+1]
+		}
+		if new_three_line_sum > last_three_line_sum {
+			part_two_count += 1
+		}
+		last_three_line_sum, new_three_line_sum = 0, 0 // Reset line sums to not interfere w/ next comparison
+	}
+	return
+}
+
+func main() {
+	answer_one_count := part1_solution(read_file())
+	answer_two_count := part2_solution(read_file())
+	fmt.Printf("Answer 1: %d\n", answer_one_count)
+	fmt.Printf("Answer 2: %d\n", answer_two_count)
+}
